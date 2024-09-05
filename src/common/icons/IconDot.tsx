adb876c64290d7c729c25d908c8d84224a1d775b
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-09-05 20:49:38 +0700

    fix: fix icon

--- src/common/icons/IconDot.tsx
+++ src/common/icons/IconDot.tsx
@@ -1,7 +1,20 @@
-
 function IconDot() {
   return (
-<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" className="rounded-full  lucide lucide-circle"><circle cx="12" cy="12" r="10"/></svg>  )
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='20'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='rounded-full lucide lucide-circle'
+    >
+      <circle cx='12' cy='12' r='10' />
+    </svg>
+  )
 }
 
 export default IconDot
--- src/common/icons/IconNl.tsx
+++ src/common/icons/IconNl.tsx
@@ -1,19 +1,17 @@
-
 function IconNl() {
   return (
-    <svg width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_814_6441)">
-    <path d="M0 0H24V18H0V0Z" fill="#21468B"/>
-    <path d="M0 0H24V12H0V0Z" fill="white"/>
-    <path d="M0 0H24V6H0V0Z" fill="#AE1C28"/>
-    </g>
-    <defs>
-    <clipPath id="clip0_814_6441">
-    <rect width="24" height="18" fill="white"/>
-    </clipPath>
-    </defs>
+    <svg width='24' height='18' viewBox='0 0 24 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <g clipPath='url(#clip0_814_6441)'>
+        <path d='M0 0H24V18H0V0Z' fill='#21468B' />
+        <path d='M0 0H24V12H0V0Z' fill='white' />
+        <path d='M0 0H24V6H0V0Z' fill='#AE1C28' />
+      </g>
+      <defs>
+        <clipPath id='clip0_814_6441'>
+          <rect width='24' height='18' fill='white' />
+        </clipPath>
+      </defs>
     </svg>
-    
   )
 }
 
