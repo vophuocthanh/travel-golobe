6a08d372f8b47edd77212d5d3a2151b9840bb5eb
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-09-11 13:07:03 +0700

    refactor: update user interface properties and add change password functionality 🐼

--- src/components/common/admin/dashboard/ContentDashboard2.tsx
+++ src/components/common/admin/dashboard/ContentDashboard2.tsx
@@ -1,75 +1,75 @@
-import { IconChina, IconGermany, IconJp, IconNl, IconRo, IconUc, IconUs } from '@/common/icons';
-import { Card, CardContent, CardTitle } from '@/components/ui/card';
-import { Cell, Pie, PieChart } from 'recharts';
+import { IconChina, IconGermany, IconJp, IconNl, IconRo, IconUc, IconUs } from '@/common/icons'
+import { Card, CardContent, CardTitle } from '@/components/ui/card'
+import { Cell, Pie, PieChart } from 'recharts'
 
 const dataInner = [
   { name: 'User A', value: 1200000 },
   { name: 'User B', value: 800000 },
   { name: 'User C', value: 645000 },
   { name: 'User D', value: 590000 },
-  { name: 'User E', value: 342000 },
-];
+  { name: 'User E', value: 342000 }
+]
 
 const dataOuter = [
   { name: 'User A', value: 600000 },
   { name: 'User B', value: 400000 },
   { name: 'User C', value: 322500 },
   { name: 'User D', value: 295000 },
-  { name: 'User E', value: 171000 },
-];
+  { name: 'User E', value: 171000 }
+]
 
-const COLORS = ['#b34700','#e65c00', '#ff751a', '#ff944d', '#ffb380'];
+const COLORS = ['#b34700', '#e65c00', '#ff751a', '#ff944d', '#ffb380']
 
 const countries = [
-  { name: 'United States', percentage: '27.5%', total: '4.5M', img: IconUs},
+  { name: 'United States', percentage: '27.5%', total: '4.5M', img: IconUs },
   { name: 'Australia', percentage: '11.2%', total: '2.3M', img: IconUc },
   { name: 'China', percentage: '9.4%', total: '2M', img: IconChina },
   { name: 'Germany', percentage: '8%', total: '1.7M', img: IconGermany },
   { name: 'Romania', percentage: '7.9%', total: '1.6M', img: IconRo },
   { name: 'Japan', percentage: '6.1%', total: '1.2M', img: IconJp },
-  { name: 'Netherlands', percentage: '5.9%', total: '1M', img: IconNl },
-  ]
+  { name: 'Netherlands', percentage: '5.9%', total: '1M', img: IconNl }
+]
 
 const users = [
   { name: 'User A', sales: '$1.2M', growth: '+8.2%', color: '#b34700', growthColor: 'text-green-600' },
   { name: 'User B', sales: '$800K', growth: '+7%', color: '#e65c00', growthColor: 'text-green-600' },
   { name: 'User C', sales: '$645K', growth: '+2.5%', color: '#ff751a', growthColor: 'text-green-600' },
   { name: 'User D', sales: '$590K', growth: '-6.5%', color: '#ff944d', growthColor: 'text-red-600' },
-  { name: 'User E', sales: '$342K', growth: '+1.7%', color: '#ffb380', growthColor: 'text-green-600' },
-];
+  { name: 'User E', sales: '$342K', growth: '+1.7%', color: '#ffb380', growthColor: 'text-green-600' }
+]
 
 export default function ContentDashboard2() {
   return (
-    <div className="grid grid-cols-2 gap-4 mt-5 bg-gray-100 ">
-      <Card className="flex flex-col col-span-1 ">
-      <CardTitle>
-        <div className="w-full p-4 px-6">
-        <h2 className="text-2xl font-semibold ">Users by Country</h2> 
-        </div>
-      </CardTitle>
-      <CardContent className="flex items-center">
-        <ul className='w-full'>
-          {countries.map((country, index) => (
-            <li key={index} className="flex items-center justify-between mb-2">
-              <div className="flex items-center">
-                <div className="w-6 h-4 mr-2">
-                  <country.img />
+    <div className='grid grid-cols-2 gap-4 mt-5 bg-gray-100 '>
+      <Card className='flex flex-col col-span-1 '>
+        <CardTitle>
+          <div className='w-full p-4 px-6'>
+            <h2 className='text-2xl font-semibold '>Users by Country</h2>
+          </div>
+        </CardTitle>
+        <CardContent className='flex items-center'>
+          <ul className='w-full'>
+            {countries.map((country, index) => (
+              <li key={index} className='flex items-center justify-between mb-2'>
+                <div className='flex items-center'>
+                  <div className='w-6 h-4 mr-2'>
+                    <country.img />
+                  </div>
+                  <span>{country.name}</span>
                 </div>
-                <span>{country.name}</span>
-              </div>
-              <div className="flex items-center">
-                <span className="mr-4">{country.percentage}</span>
-                <span>{country.total}</span>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </CardContent>
+                <div className='flex items-center'>
+                  <span className='mr-4'>{country.percentage}</span>
+                  <span>{country.total}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </CardContent>
       </Card>
-      <Card className="flex flex-col col-span-1">
-        <h2 className="p-4 mb-4 text-2xl font-semibold">Top 5 Best Selling Users</h2>
-        <div className="flex flex-row items-center w-full">
-          <div className="flex justify-center w-full mb-4">
+      <Card className='flex flex-col col-span-1'>
+        <h2 className='p-4 mb-4 text-2xl font-semibold'>Top 5 Best Selling Users</h2>
+        <div className='flex flex-row items-center w-full'>
+          <div className='flex justify-center w-full mb-4'>
             <PieChart width={200} height={200}>
               <Pie
                 data={dataOuter}
@@ -77,35 +77,29 @@ export default function ContentDashboard2() {
                 endAngle={-180}
                 innerRadius={70}
                 outerRadius={100}
-                dataKey="value"
+                dataKey='value'
                 paddingAngle={1}
               >
-                {dataOuter.map((entry, index) => (
+                {dataOuter.map((_entry, index) => (
                   <Cell key={`cell-outer-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
               </Pie>
-              <Pie
-                data={dataInner}
-                innerRadius={50}
-                outerRadius={70}
-                dataKey="value"
-                paddingAngle={1}
-              >
-                {dataInner.map((entry, index) => (
+              <Pie data={dataInner} innerRadius={50} outerRadius={70} dataKey='value' paddingAngle={1}>
+                {dataInner.map((_entry, index) => (
                   <Cell key={`cell-inner-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
               </Pie>
             </PieChart>
           </div>
-          <ul className="w-full mr-[2rem]">
+          <ul className='w-full mr-[2rem]'>
             {users.map((user, index) => (
-              <li key={index} className="flex items-center justify-between mb-2">
-                <div className="flex items-center">
-                  <div  className="w-4 h-4 mr-2 rounded-full" style={{ backgroundColor: user.color }}></div>
+              <li key={index} className='flex items-center justify-between mb-2'>
+                <div className='flex items-center'>
+                  <div className='w-4 h-4 mr-2 rounded-full' style={{ backgroundColor: user.color }}></div>
                   <span>{user.name}</span>
                 </div>
-                <div className="flex items-center">
-                  <span className="mr-4">{user.sales}</span>
+                <div className='flex items-center'>
+                  <span className='mr-4'>{user.sales}</span>
                   <span className={user.growthColor}>{user.growth}</span>
                 </div>
               </li>
--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,30 +1,30 @@
-import { useState } from 'react';
-import { DatePicker, Space } from 'antd';
-import dayjs from 'dayjs';
-import ContentDashboard2 from '@/components/common/admin/dashboard/ContentDashboard2';
-import ContentDashboard1 from '@/components/common/admin/dashboard/ContentDashboard1';
-
+import ContentDashboard2 from '@/components/common/admin/dashboard/ContentDashboard2'
+import { DatePicker, Space } from 'antd'
+import { RangePickerProps } from 'antd/es/date-picker'
+import dayjs, { Dayjs } from 'dayjs'
+import { useState } from 'react'
 
 export default function Dashboard() {
-  const [activeTab, setActiveTab] = useState('Overview');
-  const { RangePicker } = DatePicker;
-    
-  const onRangeChange = (dates, dateStrings) => {
-    if (dates) {
-      console.log('From: ', dates[0], ', to: ', dates[1]);
-      console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
-    } else {
-      console.log('Clear');
-    }
-  };
+  const [activeTab, setActiveTab] = useState('Overview')
+  const { RangePicker } = DatePicker
 
-  const menuPresets = [
-    {label: 'Last 7 Days',value: [dayjs().add(-7, 'd'), dayjs()],},
-    {label: 'Last 14 Days',value: [dayjs().add(-14, 'd'), dayjs()],},
-    {label: 'Last 30 Days',value: [dayjs().add(-30, 'd'), dayjs()],},
-    {label: 'Last 90 Days',value: [dayjs().add(-90, 'd'), dayjs()],},
-    ];
-  
+  const onRangeChange: RangePickerProps['onChange'] = (dates) => {
+    if (dates) {
+      const [start, end] = dates
+      console.log('Start Date:', start)
+      console.log('End Date:', end)
+    } else {
+      console.log('No dates selected')
+    }
+  }
+
+  const menuPresets: { label: string; value: [Dayjs, Dayjs] }[] = [
+    { label: 'Last 7 Days', value: [dayjs().subtract(7, 'd'), dayjs()] },
+    { label: 'Last 14 Days', value: [dayjs().subtract(14, 'd'), dayjs()] },
+    { label: 'Last 30 Days', value: [dayjs().subtract(30, 'd'), dayjs()] },
+    { label: 'Last 90 Days', value: [dayjs().subtract(90, 'd'), dayjs()] }
+  ]
+
   const tabs = [
     { name: 'Overview', label: 'Overview', count: null },
     { name: 'Tasks', label: 'Tasks', count: 7 },
@@ -32,63 +32,61 @@ export default function Dashboard() {
     { name: 'Team', label: 'Team', count: '99+' },
     { name: 'Reports', label: 'Reports', count: null },
     { name: 'Admin', label: 'Admin', count: null },
-    { name: 'More', label: '...', count: null },
-  ];
+    { name: 'More', label: '...', count: null }
+  ]
 
   const stats = [
-    { label: "Users Total", value: "11.8M", change: "+2,5%", changeType: "increase" },
-    { label: "New Users", value: "8.236K", change: "-1,2%", changeType: "decrease" },
-    { label: "Active Users", value: "2.352M", change: "+11%", changeType: "increase" },
-    { label: "New Users", value: "8K", change: "+5,2%", changeType: "increase" },
-  ];
+    { label: 'Users Total', value: '11.8M', change: '+2.5%', changeType: 'increase' },
+    { label: 'New Users', value: '8.236K', change: '-1.2%', changeType: 'decrease' },
+    { label: 'Active Users', value: '2.352M', change: '+11%', changeType: 'increase' },
+    { label: 'New Users', value: '8K', change: '+5.2%', changeType: 'increase' }
+  ]
+
   return (
-    <div className="w-full p-2">
-      <h1 className="mb-4 text-2xl font-bold">DASHBOARD</h1>
-      <div className="relative flex space-x-8 border-b h-[3rem]">
+    <div className='w-full p-2'>
+      <h1 className='mb-4 text-2xl font-bold'>DASHBOARD</h1>
+      <div className='relative flex space-x-8 border-b h-[3rem]'>
         {tabs.map((tab) => (
           <a
             key={tab.name}
             onClick={() => setActiveTab(tab.name)}
             className={`relative font-medium pb-2 transition-colors duration-300 cursor-pointer ${
-              activeTab === tab.name
-                ? 'text-teal-600 border-b-2 border-primary'
-                : 'text-gray-600 hover:text-teal-600	'
+              activeTab === tab.name ? 'text-teal-600 border-b-2 border-primary' : 'text-gray-600 hover:text-teal-600'
             }`}
           >
             {tab.label}
-            {tab.count !== null && (
-              <span className="ml-1 text-gray-400">
-                ({tab.count})
-              </span>
-            )}
+            {tab.count !== null && <span className='ml-1 text-gray-400'>({tab.count})</span>}
           </a>
         ))}
-      <div className='absolute flex items-cente justify-center right-[3rem] cursor-pointe'>
-      <Space direction="vertical" size={12}>
-      <RangePicker className='w-[15rem] h-[2.5rem] text-lg' presets={menuPresets} onChange={onRangeChange}/>
-      </Space>
-      </div>
-      </div>
-      <div className='mt-5'>
-        <div className="grid grid-cols-4 gap-4 bg-gray-100">
-        {stats.map((stat, index) => (
-          <div key={index} className="flex flex-col items-center p-4 bg-white rounded-lg shadow w-full h-[6rem] relative">
-            <div className='absolute left-[2rem]'>
-              <span className="flex text-lg text-gray-500">{stat.label}</span>
-              <span className="text-3xl font-bold">{stat.value}</span>
-            </div>
-            <span 
-              className={`text-sm mt-2 absolute bottom-[1rem] right-[2rem] bg-gray-200 w-12 h-6 flex items-center justify-center rounded-lg ${
-                stat.changeType === "increase" ? "text-green-500" : "text-red-500"
-              }`}
-            >
-              {stat.change}
-            </span>
-          </div>
-        ))}
+        <div className='absolute right-[3rem] flex items-center justify-center cursor-pointer'>
+          <Space direction='vertical' size={12}>
+            <RangePicker className='w-[15rem] h-[2.5rem] text-lg' presets={menuPresets} onChange={onRangeChange} />
+          </Space>
         </div>
       </div>
-        <ContentDashboard1/>
-        <ContentDashboard2/>
+      <div className='mt-5'>
+        <div className='grid grid-cols-4 gap-4 bg-gray-100'>
+          {stats.map((stat, index) => (
+            <div
+              key={index}
+              className='flex flex-col items-center p-4 bg-white rounded-lg shadow w-full h-[6rem] relative'
+            >
+              <div className='absolute left-[2rem]'>
+                <span className='text-lg text-gray-500'>{stat.label}</span>
+                <span className='text-3xl font-bold'>{stat.value}</span>
+              </div>
+              <span
+                className={`text-sm mt-2 absolute bottom-[1rem] right-[2rem] bg-gray-200 w-12 h-6 flex items-center justify-center rounded-lg ${
+                  stat.changeType === 'increase' ? 'text-green-500' : 'text-red-500'
+                }`}
+              >
+                {stat.change}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <ContentDashboard2 />
     </div>
-)}
+  )
+}
