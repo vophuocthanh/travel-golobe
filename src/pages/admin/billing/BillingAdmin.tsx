725bf7084cf3b209ac0e4034866f70b44a547505
Hùng <suga4026@gmail.com>
2024-09-19 17:00:10 +0700

    Feat: Ui Billing Admin ���

--- src/pages/admin/billing/BillingAdmin.tsx
+++ src/pages/admin/billing/BillingAdmin.tsx
@@ -1,3 +1,91 @@
+
+
+import { BillingAll } from "@/components/common/admin/billing/BillingAll";
+import { BillingFlight } from "@/components/common/admin/billing/BillingFlight";
+import { BillingHotel } from "@/components/common/admin/billing/BillingHotel";
+import { BillingTour } from "@/components/common/admin/billing/BillingTour";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function BillingAdmin() {
-  return <div>BillingAdmin</div>
+  return (
+    <div className='w-full p-2'>
+      <h1 className='mb-4 text-2xl font-bold '>BILLING</h1>
+      <div>
+      <Tabs defaultValue="all" className="w-full">
+      <TabsList className="grid w-[25rem] grid-cols-4 text-black border border-primary">
+        <TabsTrigger value="all">All</TabsTrigger>
+        <TabsTrigger value="tour">Tour</TabsTrigger>
+        <TabsTrigger value="hotel">Hotel</TabsTrigger>
+        <TabsTrigger value="flight">Flight</TabsTrigger>
+      </TabsList>
+      <TabsContent value="all">
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex w-full h-[3rem] items-center justify-between">
+              <h1>All</h1>
+              <Button>Download</Button>
+            </CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <BillingAll/>
+          </CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value="tour">
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex w-full h-[3rem] items-center justify-between">
+              <h1>Tour</h1>
+              <Button>Download</Button>
+            </CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <BillingTour/>
+          </CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value="hotel">
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex w-full h-[3rem] items-center justify-between">
+              <h1>Hotel</h1>
+              <Button>Download</Button>
+            </CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <BillingHotel/>
+          </CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value="flight">
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex w-full h-[3rem] items-center justify-between">
+              <h1>Flight</h1>
+              <Button>Download</Button>
+            </CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <BillingFlight/>
+          </CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
+      </div>
+    </div>
+  )
 }
