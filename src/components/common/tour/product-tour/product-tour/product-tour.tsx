65e413ebcb6db3459fa470f62d2edfd612e4dee2
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-09-20 21:29:15 +0700

    chore: fix UI

--- src/components/common/tour/product-tour/product-tour/product-tour.tsx
+++ src/components/common/tour/product-tour/product-tour/product-tour.tsx
@@ -1,14 +1,12 @@
-
 import { IconAdress, IconDrink } from '@/common/icons'
 import { Button } from '@/components/ui/button'
 
+import { tourApi } from '@/apis/tour.api'
+import { useQuery } from '@tanstack/react-query'
 import { ChevronDown, ChevronUp, Heart } from 'lucide-react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import StarRating from '../star-rating'
-import { useQuery } from '@tanstack/react-query'
-import { tourApi } from '@/apis/tour.api'
-import React, { useState } from 'react'
-
 
 interface Search {
   id?: string
@@ -27,18 +25,17 @@ interface TabProps {
 const ProductTour = () => {
   const [isOpenSort, setIsOpenSort] = useState<boolean>(false)
 
-  
-  const {data: getAll} = useQuery({
+  const { data: getAll } = useQuery({
     queryKey: ['getAllTour'],
     queryFn: () => tourApi.getAll()
   })
-  console.log(getAll?.data["price"], "anh");
+  console.log(getAll?.data['price'], 'anh')
   const toggleVisibilitySort = () => {
     setIsOpenSort(!isOpenSort)
     // if (isOpenSort) {
     //   getAll?.data.slice().sort((a:number, b:number) => a.price - b.price);
     // } else {
-      
+
     //     getAll?.data.slice().sort((a:number, b:number) => b.price - a.price);
     // }
   }
@@ -51,8 +48,9 @@ const ProductTour = () => {
   const Tab: React.FC<TabProps> = ({ label, description, isActive, onClick }) => (
     <div
       onClick={onClick}
-      className={`flex flex-col justify-center flex-1 px-4 ${isActive ? 'border-b-4 border-primary ' : 'border-r-2 border-transparent'
-        } transition-colors duration-300`}
+      className={`flex flex-col justify-center flex-1 px-4 ${
+        isActive ? 'border-b-4 border-primary ' : 'border-r-2 border-transparent'
+      } transition-colors duration-300`}
     >
       <p className='text-2xl text-left'>{label}</p>
       <p className='text-left text-gray-400'>{description}</p>
@@ -61,7 +59,7 @@ const ProductTour = () => {
   return (
     <div className='w-[70%]'>
       <div>
-      <div className='bg-white flex flex-row w-full h-[6rem] rounded-md border-b border-gray-300 mx-4 '>
+        <div className='bg-white flex flex-row w-full h-[6rem] rounded-md border-b border-gray-300 mx-4 '>
           {tabs.map((tab, index) => (
             <React.Fragment key={tab.label}>
               <Tab
@@ -92,64 +90,60 @@ const ProductTour = () => {
           </div>
         </div>
         <div>
-          {
-            getAll?.data.map((item: Search) => (
-              <div className='flex justify-between mb-5' key={item.id}>
-                <img src={item.images} className='mr-6 w-80 h-[360px]' alt='' />
-                <div className='p-3'>
-                  <div className='flex justify-between'>
-                    <div className='mr-5 '>
-                      <h2 className='mb-3 overflow-hidden text-4xl font-medium whitespace-pre-line text-ellipsis line-clamp-2'>{item.description}CVK Park Bosphorus Hotel Istanbul</h2>
-                      <div className='flex mb-3'>
-                        <IconAdress />
-                        <p className='ml-2 overflow-hidden whitespace-pre-line text-ellipsis line-clamp-2'>{item.name}</p>
+          {getAll?.data.map((item: Search) => (
+            <div className='flex justify-between mb-5' key={item.id}>
+              <img src={item.images} className='mr-6 w-80 h-[360px]' alt='' />
+              <div className='p-3'>
+                <div className='flex justify-between'>
+                  <div className='mr-5 '>
+                    <h2 className='mb-3 overflow-hidden text-4xl font-medium whitespace-pre-line text-ellipsis line-clamp-2'>
+                      {item.description}CVK Park Bosphorus Hotel Istanbul
+                    </h2>
+                    <div className='flex mb-3'>
+                      <IconAdress />
+                      <p className='ml-2 overflow-hidden whitespace-pre-line text-ellipsis line-clamp-2'>{item.name}</p>
+                    </div>
+                    <div className='flex justify-between w-[70%] mb-3'>
+                      <div className='flex'>
+                        <StarRating />
                       </div>
-                      <div className='flex justify-between w-[70%] mb-3'>
-                        <div className='flex'>
-                          <StarRating />
-                        </div>
-                        <div className='flex'>
-                          <IconDrink />
-                          <p className='ml-3'>
-                            <span className='font-semibold'>20+</span> Aminities
-                          </p>
-                        </div>
-                      </div>
-
-                      <div className='flex mb-4'>
-                        <Button className='mr-3 bg-white border border-primary hover:bg-slate-100'>4.2</Button>
-                        <p className='flex items-center '>
-                          <span className='text-lg font-medium'>Very Good</span> 371 reviews
+                      <div className='flex'>
+                        <IconDrink />
+                        <p className='ml-3'>
+                          <span className='font-semibold'>20+</span> Aminities
                         </p>
                       </div>
                     </div>
-                    <div>
-                      <p>starting from</p>
-                      <h2 className='font-medium text-red-500'>
-                        <span className='text-2xl'>{item.price}</span>/night
-                      </h2>
-                      <p className='text-right'>excl. tax</p>
+
+                    <div className='flex mb-4'>
+                      <Button className='mr-3 bg-white border border-primary hover:bg-slate-100'>4.2</Button>
+                      <p className='flex items-center '>
+                        <span className='text-lg font-medium'>Very Good</span> 371 reviews
+                      </p>
                     </div>
                   </div>
-                  <div className='w-full h-[25%] flex'>
-                    <div className='flex flex-row items-center w-full gap-4'>
-                      {/* className={isFavorite ? 'bg-white border border-primary' : 'bg-primary text-white w-[3.6rem] '} */}
-                      <Button
-                        
-                        
-                      >
-                        <Heart />
-                      </Button>
-                      <Link to='' className='w-[35rem]'>
-                        <Button className='w-full mx-4'>View Deals</Button>
-                      </Link>
-                    </div>
+                  <div>
+                    <p>starting from</p>
+                    <h2 className='font-medium text-red-500'>
+                      <span className='text-2xl'>{item.price}</span>/night
+                    </h2>
+                    <p className='text-right'>excl. tax</p>
+                  </div>
+                </div>
+                <div className='w-full h-[25%] flex'>
+                  <div className='flex flex-row items-center w-full gap-4'>
+                    {/* className={isFavorite ? 'bg-white border border-primary' : 'bg-primary text-white w-[3.6rem] '} */}
+                    <Button>
+                      <Heart />
+                    </Button>
+                    <Link to='/tour/all-tour/detail-view' className='w-[35rem]'>
+                      <Button className='w-full mx-4'>View Deals</Button>
+                    </Link>
                   </div>
                 </div>
               </div>
-            ))
-          }
-
+            </div>
+          ))}
         </div>
       </div>
       <div>
