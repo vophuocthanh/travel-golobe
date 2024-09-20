58230f123922b13727e8b1b52cbea1dc71b73678
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-09-20 20:30:05 +0700

    chore: fix token

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,9 @@
 import { Banner, Footer, Header, Perfect, Review } from '@/components/common'
 import CrispProvider from '@/components/crips/crisp-provirde'
+import { getAccessTokenFromLS } from '@/shared/utils/storage'
 
 export default function Home() {
+  const token = getAccessTokenFromLS()
   return (
     <div className='w-full'>
       <Header />
@@ -10,7 +12,7 @@ export default function Home() {
         <div className='items-center justify-center mx-auto max-w-7xl'>
           <Perfect />
           <Review />
-          <CrispProvider/>
+          {token ? <CrispProvider /> : null}
         </div>
         <Footer />
       </main>
