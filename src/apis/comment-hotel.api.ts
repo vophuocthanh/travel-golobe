0577a2b1369b08d689deb6d80db166ffff4dabe8
Huynh Ba Anh <huynhbaanh03112003@@gmail.com>
2024-09-21 22:09:24 +0700

    feat/call-api-comment

--- src/apis/comment-hotel.api.ts
+++ src/apis/comment-hotel.api.ts
@@ -2,11 +2,11 @@ import axiosClient from '@/apis/axios-client'
 
 export const commentHotelApi = {
   getComments(hotelId: string) {
-    const url = `/hotel/${hotelId}/reviews`
+    const url = `/hotel-comment/${hotelId}/reviews`
     return axiosClient.get(url)
   },
   addComment(hotelId: string, content: string, rating: number) {
-    const url = `/hotel/${hotelId}/reviews`
+    const url = `/hotel-comment/${hotelId}/reviews`
     return axiosClient.post(url, { hotelId, content, rating })
   }
 }
