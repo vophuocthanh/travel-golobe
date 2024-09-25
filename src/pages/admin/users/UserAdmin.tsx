aa5ac84892715b3699b0d59b342586205d21da81
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-09-25 22:09:44 +0700

    refactor: update UserAdmin component and add UserAdminDetail component

--- src/pages/admin/users/UserAdmin.tsx
+++ src/pages/admin/users/UserAdmin.tsx
@@ -1,9 +1,10 @@
-import { DataTableDemo } from '@/components/common/table/TableUser'
+import { TableUser } from '@/components/common/table/TableUser'
 
 export default function UserAdmin() {
   return (
-    <>
-      <DataTableDemo />
-    </>
+    <div className='w-full h-full'>
+      <h1 className='text-2xl font-medium'>Quản lý USERS trên hệ thống</h1>
+      <TableUser />
+    </div>
   )
 }
