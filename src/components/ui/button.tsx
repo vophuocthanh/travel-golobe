38a04a3a0625928c8f95c98be21a134485cb29b2
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-09-28 23:32:58 +0700

    fix eslint

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,27 +1,27 @@
+import { cn } from '@/shared/lib/utils'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
+import { Loader2 } from 'lucide-react'
 import * as React from 'react'
 
-import { cn } from '@/shared/lib/utils'
-import { Loader2 } from 'lucide-react'
-
-
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline'
+        default:
+          'bg-primary text-slate-50 hover:bg-primary dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90',
+        destructive: 'bg-red-500 hover:bg-red-500/90 dark:hover:bg-red-900/90',
+        outline:
+          'border border-slate-200 bg-white hover:text-slate-900 dark:border-slate-800 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+        secondary: 'bg-slate-100 dark:hover:bg-slate-800/80',
+        ghost: 'hover:bg-slate-100 dark:hover:text-slate-50',
+        link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50'
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        default: 'h-10 px-4 rounded-md py-2',
+        sm: 'h-9 px-3',
+        lg: 'h-11 px-8',
         icon: 'h-10 w-10'
       }
     },
@@ -52,4 +52,5 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 )
 Button.displayName = 'Button'
 
-export { Button }
+// eslint-disable-next-line react-refresh/only-export-components
+export { Button, buttonVariants }
