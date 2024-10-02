caab7c4c4a7520adc593a6de336e9077fb72595e
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-10-02 15:51:24 +0700

    fix: connect again api hotel comment and fix UI

--- src/components/common/hotel/hotel-detail-content/hotel-detail-overview.tsx
+++ src/components/common/hotel/hotel-detail-content/hotel-detail-overview.tsx
@@ -1,51 +1,24 @@
-import { hotelApi } from "@/apis/hotel.api";
-import { IconBling } from "@/common/icons";
-import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+interface HotelDetailOverviewProps {
+  description: string
+  averrange?: number
+  ratingStatus?: string
+  total?: number
+}
 
-
-export default function HotelDetailOverview() {
-  const { id } = useParams<{ id: string }>()
-  console.log(id,"id");
-  
-  const { data: getbyId } = useQuery({
-    queryKey: ['getById', id],
-    queryFn: () => hotelApi.getById(id),
-  })
-  console.log(getbyId,"byid");
-  
-  
+export default function HotelDetailOverview({ description, averrange, ratingStatus, total }: HotelDetailOverviewProps) {
   return (
-    <div className="flex w-full mt-5">
-      <div className="mx-auto">
+    <div className='flex w-full mt-5'>
+      <div className='mx-auto'>
         <div>
-          <hr className="w-full my-8 border-2 border-gray" />
+          <hr className='w-full my-8 border-2 border-gray' />
         </div>
-        <h2 className="mb-4 text-2xl font-semibold">Overview</h2>
-        <p className="mb-6 text-gray-700">
-          {getbyId?.description}
-        </p>
-        <div className="grid grid-cols-8 gap-4 h-[12rem]">
-          <div className="relative col-span-1 p-4 text-center rounded-lg bg-emerald-300">
-            <span className="absolute flex text-4xl font-bold top-3">4.2</span>
-            <p className="absolute flex text-lg font-semibold bottom-9">Very good</p>
-            <p className="absolute flex bottom-4">371 reviews</p>
-          </div>
-          <div className="relative col-span-1 p-4 text-center border border-gray-200 rounded-lg">
-            <IconBling />
-            <p className="absolute bottom-4">Near park</p>
-          </div>
-          <div className="relative col-span-1 p-4 text-center border border-gray-200 rounded-lg">
-            <IconBling />
-            <p className="absolute bottom-4">Near nightlife</p>
-          </div>
-          <div className="relative col-span-1 p-4 text-center border border-gray-200 rounded-lg">
-            <IconBling />
-            <p className="absolute bottom-4">Near theater</p>
-          </div>
-          <div className="relative col-span-1 p-4 text-center border border-gray-200 rounded-lg">
-            <IconBling />
-            <p className="absolute bottom-4">Clean Hotel</p>
+        <h2 className='mb-4 text-2xl font-semibold'>Overview</h2>
+        <p className='mb-6 text-gray-700'>{description}</p>
+        <div className='grid grid-cols-8 gap-4 h-[12rem]'>
+          <div className='relative col-span-1 p-4 text-center rounded-lg bg-emerald-300'>
+            <span className='absolute flex text-4xl font-bold top-3'>{averrange}</span>
+            <p className='absolute flex text-lg font-semibold bottom-9'>{ratingStatus}</p>
+            <p className='absolute flex bottom-4'>{total} reviews</p>
           </div>
         </div>
       </div>
--- src/shared/ts/interface/comment-hotel.interface.ts
+++ src/shared/ts/interface/comment-hotel.interface.ts
@@ -2,9 +2,24 @@ export interface CommentHotel {
   id: string
   content: string
   rating: number
-  createAt: string
+  createdAt: string
   updateAt: string
   userId: string
   hotelId: string
-  avatar?: string
+  user: {
+    id: string
+    name: string
+    email: string
+    avatar: string
+  }
+}
+
+export interface CommentHotelForm {
+  content: string
+  rating: number
+  hotelId: string
+}
+
+export interface ReplyCommentForm {
+  content: string
 }
