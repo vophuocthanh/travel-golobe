b633eacd5e0986f559d2915579f1ab55ecfbf9d4
Huy-Bo-24 <136145665+Huy-Bo-24@users.noreply.github.com>
2024-10-20 01:52:25 +0700

    fix/update

--- src/shared/utils/flight.shema.ts
+++ src/shared/utils/flight.shema.ts
@@ -14,16 +14,16 @@ export const CreateFlightSchema = z.object({
   image: z.string().nonempty('Image is required')
 });
 export const UpdateFlightSchema = z.object({
-  brand: z.string().nonempty('Brand is required'),
+  brand: z.string(),
   price: z.preprocess((val) => Number(val), z.number().positive().min(0, 'Price must be a positive number')),
-  start_time: z.string().nonempty('Start time is required'),
-  start_day: z.string().nonempty('Start day is required'),
-  end_day: z.string().nonempty('End day is required'),
-  end_time: z.string().nonempty('End time is required'),
-  trip_time: z.string().nonempty('Trip time is required'),
-  take_place: z.string().nonempty('Take place is required'),
-  destination: z.string().nonempty('Destination is required'),
-  trip_to: z.string().nonempty('Trip to is required'),
-  image: z.string().nonempty('Image is required'),
+  start_time: z.string(),
+  start_day: z.string(),
+  end_day: z.string(),
+  end_time: z.string(),
+  trip_time: z.string(),
+  take_place: z.string(),
+  destination: z.string(),
+  trip_to: z.string(),
+  image: z.string(),
   number_of_seats_remaining: z.preprocess((val) => Number(val), z.number().positive().min(0, 'Number of seats remaining must be a positive number')),
 });
