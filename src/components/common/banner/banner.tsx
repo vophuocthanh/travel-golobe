b94b6dfec5722790446709da6796a01d2323508e
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-11-03 13:12:23 +0700

    update payment user

--- src/components/common/banner/banner.tsx
+++ src/components/common/banner/banner.tsx
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
     <div className='relative w-full mb-32'>
-      <video height='100%' autoPlay loop muted className='flex justify-center mx-auto mt-20 max-w-7xl rounded-xl'>
+      <video autoPlay loop muted className='flex justify-center min-h-full mx-auto mt-20 max-w-7xl rounded-xl'>
         <source src='https://minio.fares.vn/mixivivu-dev/video/Mixivivuduthuyen.mp4' type='video/webm' />
       </video>
     </div>
--- src/pages/profile/components/payment-history.tsx
+++ src/pages/profile/components/payment-history.tsx
@@ -3,6 +3,7 @@ import { paymentApi } from '@/apis/payment.api'
 import { formatCurrencyVND } from '@/shared/lib/format-price'
 import { useQuery } from '@tanstack/react-query'
 import dayjs from 'dayjs'
+import { Link } from 'react-router-dom'
 
 export default function PaymentHistory() {
   const { data: getPaymentUser } = useQuery({
@@ -15,41 +16,64 @@ export default function PaymentHistory() {
         <h1 className='mt-6 text-3xl font-bold'>Lịch sử thanh toán</h1>
         <h1 className='mt-6 text-2xl font-bold'>Tổng: {getPaymentUser?.total}</h1>
       </div>
-      {getPaymentUser?.data?.map((item: any) => (
-        <div
-          key={item.id}
-          className='flex items-center justify-between w-full gap-6 px-4 py-6 my-5 bg-white border border-gray-300 rounded-lg shadow-md'
-        >
-          <div className='flex items-center gap-6'>
-            <div className='flex flex-col gap-2'>
-              <h1 className=''>
-                <span className='text-xl font-medium'>Id thanh toán:</span> {item.id}
-              </h1>
-              <h1>
-                <span className='text-xl font-medium'>Phương thức thanh toán:</span> {item.paymentMethod}
-              </h1>
+      {getPaymentUser?.data?.length ?? 0 > 0 ? (
+        getPaymentUser?.data?.map((item: any) => (
+          <Link
+            to={
+              item.tour
+                ? `/tour/${item.tour.id}`
+                : item.flightCrawls
+                ? `/flight/${item.flightCrawls.id}`
+                : item.hotelCrawls
+                ? `/hotel/${item.hotelCrawls.id}`
+                : '#'
+            }
+            key={item.paymentId}
+          >
+            <div className='flex items-center w-full gap-6 px-4 py-6 my-5 bg-white border border-gray-300 rounded-lg shadow-md'>
+              <img
+                src={item.tour?.image || item.flightCrawls?.image || item.hotelCrawls?.image || 'default-image-url'}
+                alt=''
+                className='w-24 h-24 rounded-md'
+              />
+              <div className='flex items-center gap-6'>
+                <div className='flex flex-col gap-2'>
+                  <h1 className=''>
+                    <span className='text-lg font-medium'>
+                      {item.tour?.name ||
+                        item.flightCrawls?.brand ||
+                        item.hotelCrawls?.hotel_names ||
+                        'No name available'}
+                    </span>
+                  </h1>
+                  <h1>
+                    <span className='text-base font-medium'>Phương thức thanh toán:</span> {item?.paymentMethod}
+                  </h1>
+                  <h1>
+                    <span className='text-xl font-medium'>Ngày thanh toán:</span>{' '}
+                    {dayjs(item.createdAt).format('HH:mm:ss, DD-MM-YYYY')}
+                  </h1>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <h1 className='text-xl font-medium'>Tổng tiền: {formatCurrencyVND(item.totalAmount)}</h1>
+                  <span>
+                    <span className='text-xl font-medium'>Trạng thái:</span>
+                    <span
+                      className={`px-2 py-1 text-white rounded-md ${
+                        item.status === 'COMPLETED' ? 'bg-green-300' : 'bg-yellow-300'
+                      }`}
+                    >
+                      {item.status}
+                    </span>
+                  </span>
+                </div>
+              </div>
             </div>
-            <div className='flex flex-col gap-2'>
-              <h1>
-                {' '}
-                <span className='text-xl font-medium'>Ngày thanh toán:</span>{' '}
-                {dayjs(item.createdAt).format('HH:mm:ss, DD-MM-YYYY')}
-              </h1>
-              <span>
-                <span className='text-xl font-medium'>Trạng thái:</span>
-                <span
-                  className={`px-2 py-1 text-white rounded-md ${
-                    item.status === 'COMPLETED' ? 'bg-green-300' : 'bg-yellow-300'
-                  }`}
-                >
-                  {item.status}
-                </span>
-              </span>
-            </div>
-          </div>
-          <h1 className='text-xl font-medium'>Tổng tiền: {formatCurrencyVND(item.amount)}</h1>
-        </div>
-      ))}
+          </Link>
+        ))
+      ) : (
+        <h1 className='flex items-center justify-center text-xl font-medium'>Chưa thanh toán đơn hàng nào</h1>
+      )}
     </div>
   )
 }
