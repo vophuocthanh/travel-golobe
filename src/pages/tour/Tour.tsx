ab2738ecbbbcc2fc3edf10c34709543188f2bbc9
levananh202ht <levananh202@evananh202gmail.com>
2024-11-25 08:24:01 +0700

    feat(PG-reponsive): tour-home

--- src/pages/tour/Tour.tsx
+++ src/pages/tour/Tour.tsx
@@ -9,9 +9,9 @@ export default function Tour() {
       <Header />
       <div className="relative w-full ">
         <img src={banner_tour} alt="" className="object-cover w-full h-screen " />
-        <div className="absolute inset-0 flex items-center justify-center w-[650px] flex-col bg-gradient-to-r from-blue-500/50 to-transparent">
-          <div className="ml-32">
-            <h1 className="text-5xl text-white w-[28rem]">{t('travelwhishlist')}</h1>
+        <div className="absolute inset-0 flex items-center justify-center w-[50%] max-md:w-[80%] flex-col bg-gradient-to-r from-blue-500/50 to-transparent">
+          <div className="ml-32 ">
+            <h1 className="text-5xl max-lg:text-3xl text-white w-[28rem] max-sm:w-[20rem]">{t('travelwhishlist')}</h1>
             <p className="text-white ">{t('Special')}</p>
           </div>
         </div>
@@ -34,7 +34,7 @@ export default function Tour() {
           </div>
         </div> */}
       </div>
-      <div className="w-full h-[280px] "></div>
+      <div className="w-full h-[280px] max-xl:h-[120px] max-md:h-[80px] "></div>
 
       <SearchTour />
       <IntoTour />
