f6bf991a9a5ac90edcb13c063dd469c45730f71b
Huynh Ba Anh <huynhbaanh03112003@@gmail.com>
2024-11-29 20:39:32 +0700

    fix responsive hotel

--- src/pages/home-stay/HomeStay.tsx
+++ src/pages/home-stay/HomeStay.tsx
@@ -14,9 +14,9 @@ export default function HomeStay() {
         <div className="mx-auto container lg:mx-[5rem]">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 pt-5 bg-gray-100">
             {/* Booking Form and Search Button */}
-            <div className="flex flex-col md:col-span-3 items-center justify-center gap-10 p-6 mt-40 bg-white border border-gray-300 shadow-lg rounded-xl">
+            <div className="flex flex-col lg:flex-row md:col-span-3 items-center justify-center p-6 mt-40 bg-white border border-gray-300 shadow-lg rounded-xl">
               <BookingForm />
-              <Button className="px-6 py-2 font-semibold text-white transition-all duration-300 ease-in-out rounded-lg shadow-md bg-primary hover:bg-primary-dark">
+              <Button className="px-6 py-2 h-[56px] w-[273px] lg:w-[150px] font-semibold text-white rounded-lg bg-primary hover:bg-primary-dark">
                 <Search />
               </Button>
             </div>
