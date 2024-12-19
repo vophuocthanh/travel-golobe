6d25b56af153576750162361fe55fb900abd3c09
Hùng <suga4026@gmail.com>
2024-12-19 21:47:33 +0700

    Fix: text home

--- src/shared/lib/data-type.ts
+++ src/shared/lib/data-type.ts
@@ -60,63 +60,63 @@ export const dataPerfect = [
     id: '1',
     image:
       'https://images.unsplash.com/photo-1483683804023-6ccdb62f86ef?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Paris',
-    title: 'Paris, France'
+    name: 'Nha Trang',
+    title: 'TP Nha Trang, Việt Nam'
   },
   {
     id: '2',
     image:
       'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'London',
-    title: 'London, UK'
+    name: 'Đà Nẵng',
+    title: 'TP Đà Nẵng, Việt Nam'
   },
   {
     id: '3',
     image:
       'https://plus.unsplash.com/premium_photo-1700828284504-02bd8d5fb2d4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'New York',
-    title: 'New York, USA'
+    name: 'Hạ Long',
+    title: 'TP Hạ Long, Việt Nam'
   },
   {
     id: '4',
     image:
       'https://images.unsplash.com/photo-1487662701465-ee09afb4e1fa?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Tokyo',
-    title: 'Tokyo, Japan'
+    name: 'Hội An',
+    title: 'TP Hội An, Việt Nam'
   },
   {
     id: '5',
     image:
       'https://images.unsplash.com/photo-1516483638261-f4dbaf036963?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Sydney',
-    title: 'Sydney, Australia'
+    name: 'Hà Nội',
+    title: 'TP Hà Nội, Việt Nam'
   },
   {
     id: '6',
     image:
       'https://plus.unsplash.com/premium_photo-1681487839546-e0f7d8b1e369?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Singapore',
-    title: 'Singapore, Singapore'
+    name: 'TP Hồ Chí Minh',
+    title: 'TP Hồ Chí Minh, Việt Nam'
   },
   {
     id: '7',
     image:
       'https://images.unsplash.com/photo-1483247416020-58799b6de4c1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Dubai',
-    title: 'Dubai, UAE'
+    name: 'Phú Quốc',
+    title: 'Đảo Phú Quốc, Việt Nam'
   },
   {
     id: '8',
     image:
       'https://images.unsplash.com/photo-1503221043305-f7498f8b7888?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Rome',
-    title: 'Rome, Italy'
+    name: 'Sa Pa',
+    title: 'Thị trấn Sa Pa, Việt Nam'
   },
   {
     id: '9',
     image:
       'https://images.unsplash.com/photo-1506012787146-f92b2d7d6d96?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Cairo',
-    title: 'Cairo, Egypt'
+    name: 'Hà Giang',
+    title: 'Hà Giang, Việt Nam'
   }
 ]
